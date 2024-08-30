77bde725222022d4bb9dd7c3583e1e53542673eb
Altiorem <rennzo323@tutanota.com>
2024-08-29 20:59:32 -0500

    Experimenting

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CC = gcc
 OBJECTS = src/main.c
 
 all:
-	$(CC) -o sushi $(OBJECTS) $(CFLAGS)
+	$(CC) -o sushi $(OBJECTS) $(CFLAGS)  -lreadline
 
 clear:
 	rm sushi
--- src/main.c
+++ src/main.c
@@ -4,71 +4,59 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <sys/utsname.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-static void cd(char **args){
-
+static void cd(char **args) {
     if (args[1] == NULL) {
         fprintf(stderr, "Expected argument to \"cd\"\n");
-
     } else {
-
         if (chdir(args[1]) != 0) {
             perror("cd");
         }
     }
 }
 
-static bool execute(char **args){
-
+static bool execute(char **args) {
     pid_t pid, wpid;
     int status;
 
     pid = fork();
     if (pid == 0) {
-
         if (execvp(args[0], args) == -1) {
-
-            fprintf(stderr, "execvp:  error\n");
-
+            perror("execvp");
+            exit(EXIT_FAILURE);
         }
-        exit(0);
     } else if (pid < 0) {
-
-        fprintf(stderr, "execvp:  error\n");
-
+        perror("fork");
     } else {
-                
-        wpid = waitpid(pid, &status, WUNTRACED);
-
-        while (!WIFEXITED(status) && !WIFSIGNALED(status)){
-
+        do {
             wpid = waitpid(pid, &status, WUNTRACED);
-
-        }
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
-  return 1;
+    return true;
 }
 
-// Execute a program or Shell Builtins
-static bool launch(char **args){
-
-    if (strcmp(args[0] , "exit" )== 0) {
-        return 0;
+static bool launch(char **args) {
+    if (args[0] == NULL) {
+        return true; // Empty command
     }
 
-    if (strcmp(args[0] , "cd" )== 0) {
+    if (strcmp(args[0], "exit") == 0) {
+        return false;
+    }
+
+    if (strcmp(args[0], "cd") == 0) {
         cd(args);
-        return 1;
+        return true;
     }
 
     return execute(args);
 }
 
-static char **splitLines(char *line){
-
+static char **splitLines(char *line) {
     const int TOK_BUFSIZE = 64;
     const char *TOK_DELIM = " \t\r\n\a";
     int bufsize = TOK_BUFSIZE;
@@ -77,24 +65,22 @@ static char **splitLines(char *line){
     char *token;
 
     if (!tokens) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(0);
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
     }
 
     token = strtok(line, TOK_DELIM);
-
-    while (token != NULL){
+    while (token != NULL) {
         tokens[position] = token;
         position++;
 
         if (position >= bufsize) {
-
             bufsize += TOK_BUFSIZE;
             tokens = realloc(tokens, bufsize * sizeof(char*));
 
             if (!tokens) {
-                fprintf(stderr, "malloc: allocation error\n");
-                exit(0);
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
             }
         }
 
@@ -105,67 +91,57 @@ static char **splitLines(char *line){
     return tokens;
 }
 
-static char *readLine(void) {
-    char *line = NULL;
-    size_t bufsize = 0; 
+static bool readInput(char **line) {
+    *line = readline("> ");
+    if (*line == NULL) {
+        perror("readline");
+        exit(EXIT_FAILURE);
+    }
 
-    if (getline(&line, &bufsize, stdin) == -1){
-        if (feof(stdin)) {
-            perror("Getline feof(stdin)");
-            exit(1);  
-        } else  {
-            fprintf(stderr, "readline:  error\n");
-            exit(0);
+    if (strlen(*line) == 0) { // Handle empty line
+        free(*line);
+        return false;
+    }
+
+    add_history(*line);
+    return true;
+}
+
+static void sushiLoop(char *username, char *hostname) {
+    char *line = NULL;
+    char **args;
+    bool shStatus = true;
+
+    while (shStatus) {
+        printf("\033[1;31m%s\033[1;0m@\033[1;31m%s\033[1;0m: ", username, hostname);
+
+        if (readInput(&line)) {
+            args = splitLines(line);
+            shStatus = launch(args);
+            free(args);
         }
-    }
 
-    return line;
-
-}
-
-static void sushiLoop(char *username, char * hostname){
-    char *line = NULL;
-    char **args = NULL;
-    
-    bool shStatus = 1; // If 0, exit shell
-
-    while (shStatus){
-
-        printf("%s@%s:",username, hostname);
-        printf(">");
-
-        line = readLine();
-        args = splitLines(line);
-        shStatus = launch(args);
         free(line);
-        free(args);
-           
+        line = NULL; // Reset line to NULL after free
     }
-    
 }
-
 
 int main(int argc, char** argv) {
-    char *hostname;
+    char hostname[256];
     char *username;
-    const int BUF_DELIM = 64;
-    int bufsize = 64;
 
     username = getlogin();
     if (username == NULL) {
         perror("getlogin");
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    hostname = malloc( sizeof(char) * bufsize );
-
-    while(gethostname(hostname, sizeof(hostname)) != 0){
-        
-            bufsize += BUF_DELIM;
-            hostname = realloc(hostname, sizeof (char) * bufsize);
-
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        perror("gethostname");
+        return EXIT_FAILURE;
     }
+
     sushiLoop(username, hostname);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
